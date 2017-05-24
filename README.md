a2ea22e68f9b19026607435e41a9a7e7d5bce63d
Abass Sesay <bassco10@gmail.com>
2017-05-23 22:27:12 -0400

    Code Refactor
    Signed-off-by: Abass Sesay <Abass_Sesay2@cable.comcast.com>

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ This project stems from a Network class project in which we had to implement bot
 The original assignment was done in Python. This project would be a translation of that project using 
 best practices (as much as possible) of GO. 
 
-PS this is the first full fledge GO project that I have taken on.
+PS this is the first full fledged GO project that I have taken on.
+
+``go run client.go <server ip> <port>``
--- client.go
+++ client.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strconv"
@@ -10,25 +12,34 @@ import (
 )
 
 type Server struct {
-	host, port, message, d_message, data_host, data_port string
-	ctrl_conn, data_conn                                 net.Conn
-	data_socket                                          net.Listener
-	passive                                              bool
+	host, port, message, r_code, data_host, data_port string
+	ctrl_conn, data_conn                              net.Conn
+	data_socket                                       net.Listener
+	passive                                           bool //passive is true if the user sets up data port with PASV or EPSV
 }
 
 func (s *Server) address() string {
 	return s.host + ":" + s.port
 }
 
-func (s *Server) connect() {
+func (s *Server) connect() error {
 	s.ctrl_conn, _ = net.Dial("tcp", s.address())
 	s.getResponse()
+	switch s.r_code {
+	case "421":
+		return FTPError{level: "fatal", what: s.message}
+	case "220":
+		return nil
+	}
+	return FTPError{level: "fatal", what: s.message}
 }
 
 func (s *Server) getResponse() {
 	buf := make([]byte, 1024)
 	n, _ := s.ctrl_conn.Read(buf)
-	s.message = string(buf[:n])
+	s.message = string(buf[:n-2])
+	_code_and_message := strings.Split(s.message, " ")
+	s.r_code = _code_and_message[0]
 }
 
 func (s *Server) send(msg string) {
@@ -57,6 +68,124 @@ func (s *Server) setDataConnPRTE(host string) {
 	} else {
 		s.data_socket, _ = net.Listen("tcp", ":"+s.data_port)
 	}
+}
+
+func processCommand(cli string) {
+	var command, args string
+	_split := strings.SplitN(cli, " ", 2)
+	command = _split[0]
+	if len(_split) > 1 {
+		args = _split[1]
+	}
+	_func, ok := command2Func[command]
+	if !ok {
+		fmt.Println("Sorry command is not currently supported")
+		return
+	}
+
+	_func(args)
+}
+
+func USER(args string) {
+	srvr.send("USER " + args)
+}
+
+func PASS(args string) {
+	srvr.send("PASS " + args)
+}
+
+func QUIT(args string) {
+	srvr.send("QUIT")
+	os.Exit(0)
+}
+
+func PWD(args string) {
+	srvr.send("PWD")
+}
+
+func PORT(args string) {
+	srvr.passive = false
+	_host_upper_lower := strings.Split(args, ",")
+	host, upper, lower := _host_upper_lower[:4], _host_upper_lower[4], _host_upper_lower[5]
+	srvr.setDataConnPRT(strings.Join(host, ","), upper, lower)
+	srvr.send("PORT " + args)
+	return
+}
+
+func LIST(args string) {
+	var buf bytes.Buffer
+	srvr.send("LIST " + args)
+	if !srvr.passive {
+		srvr.data_conn, _ = srvr.data_socket.Accept()
+	}
+	defer srvr.data_conn.Close()
+	fmt.Print(srvr.message)
+	io.Copy(&buf, srvr.data_conn)
+	_bytes_read := len(buf.String())
+	fmt.Println(buf.String()[:_bytes_read-2])
+	srvr.getResponse()
+}
+
+func PASV(args string) {
+	srvr.passive = true
+	srvr.send("PASV " + args)
+	_upper := strings.Index(srvr.message, "(") + 1
+	_lower := strings.Index(srvr.message, ")")
+	address := srvr.message[_upper:_lower]
+	_address := strings.Split(address, ",")
+	host, upper, lower := _address[0], _address[1], _address[2]
+	srvr.setDataConnPRT(string(host), string(upper), string(lower))
+}
+
+func HELP(args string) {
+	srvr.send("HELP")
+}
+
+func CDUP(args string) {
+	srvr.send("CDUP")
+}
+
+func CWD(args string) {
+	srvr.send("CWD " + args)
+}
+
+func NOOP(args string) {
+	srvr.send("NOOP")
+}
+
+func RETR(args string) {
+	srvr.send("RETR " + args)
+	if !srvr.passive {
+		srvr.data_conn, _ = srvr.data_socket.Accept()
+	}
+	fmt.Print("Response: " + srvr.message)
+	_upper := strings.Index(srvr.message, "(") + 1
+	_lower := strings.Index(srvr.message, " bytes)")
+	_size, _ := strconv.Atoi(srvr.message[_upper:_lower])
+	buf := make([]byte, _size)
+	n, _ := srvr.data_conn.Read(buf)
+	file, _ := os.Create(args)
+	file.Write(buf[:n-2])
+	srvr.getResponse()
+}
+
+func EPSV(args string) {
+	srvr.passive = true
+	srvr.send("EPSV")
+	_upper := strings.Index(srvr.message, "(|") + 2
+	_lower := strings.Index(srvr.message, "|)")
+	_address := srvr.message[_upper:_lower]
+	_prtcl_host_port := strings.Split(_address, "|")
+	srvr.data_port = _prtcl_host_port[2]
+	srvr.setDataConnPRTE(_prtcl_host_port[1])
+}
+
+type FTPError struct {
+	what, level string
+}
+
+func (e FTPError) Error() string {
+	return fmt.Sprintf("%v: %v", e.level, e.what)
 }
 
 var (
@@ -100,124 +229,16 @@ func main() {
 
 	srvr = Server{host: host, port: port}
 	fmt.Println("Server has address " + srvr.address())
-	srvr.connect()
-	fmt.Print("Server message " + srvr.message)
+	if err := srvr.connect(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(srvr.message)
 
 	for {
 		fmt.Print("command-with-args> ")
 		reader.Scan()
 		processCommand(reader.Text())
-		fmt.Print("Response: " + srvr.message)
+		fmt.Println(srvr.message)
 	}
-}
-
-func processCommand(cli string) {
-	var command, args string
-	_split := strings.SplitN(cli, " ", 2)
-	command = _split[0]
-	if len(_split) > 1 {
-		args = _split[1]
-	}
-	_func, ok := command2Func[command]
-	if !ok {
-		fmt.Println("Sorry command is not currently supported")
-		return
-	}
-
-	_func(args)
-}
-
-func USER(args string) {
-	srvr.send("USER " + args)
-}
-
-func PASS(args string) {
-	srvr.send("PASS " + args)
-}
-
-func QUIT(args string) {
-	srvr.send("QUIT " + args)
-	os.Exit(0)
-}
-
-func PWD(args string) {
-	srvr.send("PWD " + args)
-}
-
-func PORT(args string) {
-	srvr.passive = false
-	_host_upper_lower := strings.Split(args, ",")
-	host, upper, lower := _host_upper_lower[:4], _host_upper_lower[4], _host_upper_lower[5]
-	srvr.setDataConnPRT(strings.Join(host, ","), upper, lower)
-	srvr.send("PORT " + args)
-	return
-}
-
-func LIST(args string) {
-	srvr.send("LIST " + args)
-	if !srvr.passive {
-		srvr.data_conn, _ = srvr.data_socket.Accept()
-	}
-	fmt.Print("Response: " + srvr.message)
-	buf := make([]byte, 1024)
-	n, _ := srvr.data_conn.Read(buf)
-	fmt.Println(string(buf[:n]))
-	srvr.getResponse()
-}
-
-func PASV(args string) {
-	srvr.passive = true
-	srvr.send("PASV " + args)
-	_upper := strings.Index(srvr.message, "(") + 1
-	_lower := strings.Index(srvr.message, ")")
-	address := srvr.message[_upper:_lower]
-	_address := strings.Split(address, ",")
-	host, upper, lower := _address[0], _address[1], _address[2]
-	srvr.setDataConnPRT(string(host), string(upper), string(lower))
-}
-
-func HELP(args string) {
-	srvr.send("HELP " + args)
-}
-
-func CDUP(args string) {
-	srvr.send("CDUP " + args)
-}
-
-func CWD(args string) {
-	srvr.send("CWD " + args)
-}
-
-func NOOP(args string) {
-	srvr.send("NOOP " + args)
-}
-
-func RETR(args string) {
-	srvr.send("RETR " + args)
-	if !srvr.passive {
-		srvr.data_conn, _ = srvr.data_socket.Accept()
-	}
-	fmt.Print("Response: " + srvr.message)
-	_upper := strings.Index(srvr.message, "(") + 1
-	_lower := strings.Index(srvr.message, " bytes)")
-	_size, _ := strconv.Atoi(srvr.message[_upper:_lower])
-	buf := make([]byte, _size)
-	n, _ := srvr.data_conn.Read(buf)
-	file, _ := os.Create(args)
-	file.Write(buf[:n])
-	srvr.getResponse()
-}
-
-func EPSV(args string) {
-	srvr.passive = true
-	srvr.send("EPSV " + args)
-	fmt.Println(srvr.message)
-	_upper := strings.Index(srvr.message, "(|") + 2
-	_lower := strings.Index(srvr.message, "|)")
-	_address := srvr.message[_upper:_lower]
-	fmt.Println(_address)
-	_prtcl_host_port := strings.Split(_address, "|")
-	srvr.data_port = _prtcl_host_port[2]
-	fmt.Println(srvr.data_port)
-	srvr.setDataConnPRTE(_prtcl_host_port[1])
 }
